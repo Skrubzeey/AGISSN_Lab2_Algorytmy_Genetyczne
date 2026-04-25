47a67fbf20b840517bccaceec00d70791cc0c71a
Your Name <jednog000@pbs.edu.pl>
2026-04-25 19:30:47 +0200

    Stworzenie logiki dla enkodowania informacji

---
+++ utils.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+class Encoding:
+    def initialize(self, population_size):
+        pass
+
+    @staticmethod
+    def repair(self, individual):
+        return individual
+
+
+class RealEncoding(Encoding):
+    def __int__(self, dimension, bounds):
+        self.dimensions = dimension
+        self.bounds = bounds
+
+    def initialize(self, population_size):
+        low, high = self.bounds
+        return np.random.uniform(low, high,
+                                 (population_size, self.dimensions))
+
+
+class PermutationEncoding(Encoding):
+    def __int__(self, n):
+        self.n = n
+
+    def initialize(self, population_size):
+        return np.array([
+            np.random.permutation(self.n)
+            for _ in range(population_size)
+        ])
+
