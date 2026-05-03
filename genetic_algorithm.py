372b6c018e58083495bc395ba71b4d2fcf37d4b2
Skrubzeey <jednog000@pbs.edu.pl>
2026-05-03 20:15:38 +0200

    Pełna implementacja bazowej klasy GA

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -4,11 +4,12 @@ from utils import Encoding, RealEncoding, PermutationEncoding
 
 
 class BaseAG(ABC):
-    def __init__(self, population_size, fittness_func, crossover_prob, mutation_prob):
+    def __init__(self, population_size, fittness_func, crossover_prob, mutation_prob, selection_method):
         self.population_size = population_size
         self.fitness_func = fittness_func
-        self.crossover_prov = crossover_prob
+        self.crossover_prob = crossover_prob
         self.mutation_prob = mutation_prob
+        self.selection = selection_method
 
     @abstractmethod
     def initialize_population(self):
@@ -22,6 +23,35 @@ class BaseAG(ABC):
     def mutation(self, index):
         pass
 
+    def evaluate(self, population):
+        return np.array([self.fitness_func(idx) for idx in population])
 
+    def run(self, generations):
+        population = self.initialize_population()
+        history = []
+
+        for _ in range(generations):
+            fitness = self.evaluate(population)
+
+            new_population = []
+
+            while len(new_population) < self.population_size:
+                p1 = self.selection(population, fitness)
+                p2 = self.selection(population, fitness)
+
+                if np.random.rand() < self.crossover_prob:
+                    c1, c2 = self.crossover(p1, p2)
+                else:
+                    c1, c2 = p1.copy(), p2.copy()
+
+                c1 = self.mutation(c1)
+                c2 = self.mutation(c2)
+
+                new_population.extend([c1, c2])
+
+            population = np.array(new_population[:self.population_size])
+            history.append(np.min(fitness))
+
+        return population, history
 
 
